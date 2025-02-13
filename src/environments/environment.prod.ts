65d4e08056c227e8ab877c3d65b9eb0ae8a8cb07
Dhanush V <dhanush.v0724@gmail.com>
2025-02-13 21:21:10 +0530

    asih got some shit on

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBtAu0SZRi6WpxVhA6MUV7Vs7SL-NYMq8o",
+      authDomain: "seednestle-93a3e.firebaseapp.com",
+      projectId: "seednestle-93a3e",
+      storageBucket: "seednestle-93a3e.appspot.com",
+      messagingSenderId: "157068886012",
+      appId: "1:157068886012:web:0ea6372f65952cde23bc79"
+    }
+  };
+  
